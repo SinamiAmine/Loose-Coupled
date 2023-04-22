369cb9c9a5c5e2235d74881a837a948f950c422a
dev <sinami.pluriel@gmail.com>
2023-04-22 13:29:07 +0100

    Titghly Coupled Concept

---
+++ src/main/java/com/domestic/learnspringframework/game/SuperContraGame.java
@@ -0,0 +1,18 @@
+package com.domestic.learnspringframework.game;
+
+public class SuperContraGame {
+
+    public void up(){
+        System.out.println("up");
+    }
+    public void down(){
+        System.out.println("Sit down");
+    }
+    public void left(){
+        System.out.println("Go back");
+    }
+    public void right(){
+        System.out.println("Shoot a bullet");
+    }
+}
+
